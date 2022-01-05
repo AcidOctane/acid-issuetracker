31ccc7b9cc57c63541d4a5493a70248c3db430dd
ArtemSydorenko <s1dneyoctane@gmail.com>
2022-01-05 12:50:10 +0200

    Post fixed Add get

--- db-connection.js
+++ db-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const db = mongoose.connect(process.env.DB, {
-  useUnifiedTopology: true;
-  useNewUrlParseer: true;
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 
 module.exports = db;
